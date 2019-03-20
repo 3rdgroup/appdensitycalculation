8760b812533e803c5c537ad3c5dee88f9f27b971
Nathan Kuo <sckuo@globe.com.ph>
2019-03-20 10:42:15 +0800

    Final Answer for #3

--- BarangayDensity.R
+++ BarangayDensity.R
@@ -1,1 +1,31 @@
-BarangayDensity
+# GROUP 3 (Alejandro, Dizon, Kuo, Matias, Pena)
+
+#Use Library dplyr
+
+library(dplyr)
+
+# Load the datasets
+populationData = read.csv(file="population.csv", head=TRUE)
+regionAreaData = read.csv(file="regionarea.csv", head=TRUE)
+DT<-data.table(populationData)
+
+
+populationDataGroupedByBarangay = group_by(populationData, Region,CityProvince,Barangay)
+populationDataGroupedByBarangayWithCount = summarize(populationDataGroupedByBarangay, population = sum(Population))
+
+
+mergedPopulationDataAndRegionData = merge(populationDataGroupedByBarangayWithCount, regionAreaData, by = "Region")       
+
+init=DT[,.(n=length(Barangay)),by=Region]
+tallyOfBarangayPerRegion=init[order(Region)]
+
+mergedPopulationDataAndRegionDataAndTally = merge(mergedPopulationDataAndRegionData, tallyOfBarangayPerRegion, by = "Region")       
+
+
+mergedPopulationDataAndRegionDataAndTallyWithBarangayArea = mutate(mergedPopulationDataAndRegionDataAndTally, landAreaOfBarangay = Area/n)
+finalDataSet = mutate(mergedPopulationDataAndRegionDataAndTallyWithBarangayArea, populationDensity = population/landAreaOfBarangay)
+
+
+output = arrange(finalDataSet, desc(populationDensity))
+
+topFiveBarangayWithHighestDensity = head(output, n=15)
